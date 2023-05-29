aa484b6bb33fb44d71c49b95d8a85921927a7d12
Ajmal Shahabudeen <115772240+ajmalshahabudeen@users.noreply.github.com>
2023-05-29 17:10:29 +0000

    Created Models | Created a Register page - 01

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+
+class Products(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(max_length=255)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+        
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    created_at = models.DateField(auto_now_add=True)
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='items'
+    )
+    product = models.ForeignKey(
+        Products, on_delete=models.CASCADE
+        )
+    quantity = models.PositiveBigIntegerField()
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect("home")
+        else:
+            form = UserCreationForm()
+            
+        return render(request, "users/register.html", {"form": form})
