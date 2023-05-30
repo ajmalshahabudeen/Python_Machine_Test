b2c8316b32f92a4f9f1743ef702899a53b9ae2b2
Ajmal Shahabudeen <115772240+ajmalshahabudeen@users.noreply.github.com>
2023-05-30 12:21:26 +0000

    can see cart

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns = [
     path("", views.product, name="product"),
-    path("cart", views.cart, name="cart"),
+    path("cart/", views.cart, name="cart"),
     path("add_to_cart", views.add_to_cart, name= "add"),
     # path("confirm_payment/<str:pk>", views.confirm_payment, name="add"),
 ]
